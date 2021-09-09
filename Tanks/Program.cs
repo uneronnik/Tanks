2c8d96a8027651a96011eb6a300dd366f669dd05
uneronnik <NIKI.FOMENKO.7@GMAIL.COM>
2021-09-09 12:57:33 +0300

    1.0

---
+++ Tanks/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tanks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tank[] T34Tanks = new Tank[5];
+            for(int i = 0; i < T34Tanks.Length; i++)
+            { 
+                T34Tanks[i] = new Tank("T34");
+                Console.WriteLine(T34Tanks[i].dataToString());
+            }
+
+            Tank[] PanteraTanks = new Tank[5];
+            for (int i = 0; i < PanteraTanks.Length; i++)
+            {
+                PanteraTanks[i] = new Tank("Pantera");
+                Console.WriteLine(PanteraTanks[i].dataToString());
+            }
+
+            int T34Wins = 0;
+            int PanteraWins = 0;
+
+            for (int i = 0; i < T34Tanks.Length; i++)
+            {
+                if (T34Tanks[i] * PanteraTanks[i] == true)
+                    T34Wins++;
+                else
+                    PanteraWins++;
+            }
+
+            if (T34Wins > PanteraWins)
+                Console.WriteLine("Танки T34 победили!");
+            else
+                Console.WriteLine("Танки Pantera победили!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tanks/Tank.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tanks
+{
+    class Tank
+    {
+        private string _name;
+        private int _ammunition;
+        private int _armorLevel;
+        private int _maneuverabilityLevel;
+
+        public Tank(string name)
+        {
+            Random random = new Random();
+
+            _name = name;
+            _ammunition = random.Next(0, 100);
+            _armorLevel = random.Next(0, 100);
+            _maneuverabilityLevel = random.Next(0, 100);
+        }
+
+        public string Name { get => _name; }
+        public int Ammunition { get => _ammunition; }
+        public int ArmorLevel { get => _armorLevel; }
+        public int ManeuverabilityLevel { get => _maneuverabilityLevel; }
+
+        public string dataToString()
+        {
+            string data = "";
+            data += _name;
+            data += "\n";
+
+            data += $"Боекомплект: {_ammunition}";
+            data += "\n";
+
+            data += $"Уровень брони: {_armorLevel}";
+            data += "\n";
+
+            data += $"Уровень манёвренности: {_maneuverabilityLevel}";
+            data += "\n";
+
+            return data;
+        }
+
+        public static bool operator*(Tank firstTank, Tank secondTank)
+        {
+            int exceedingParametersNumber = 0;
+
+            if (firstTank.Ammunition > secondTank.Ammunition)
+                exceedingParametersNumber++;
+
+            if (firstTank.ArmorLevel > secondTank.ArmorLevel)
+                exceedingParametersNumber++;
+
+            if (firstTank.ManeuverabilityLevel > secondTank.ManeuverabilityLevel)
+                exceedingParametersNumber++;
+
+            return (exceedingParametersNumber > 1)? true : false;
+        }
+    }
+}
